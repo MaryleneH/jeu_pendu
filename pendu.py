3e9716d216715dc9d41aa6e42472e4fc94cf9696
MaryleneH <marylene.henry@gmail.com>
2024-07-06 13:21:10 +0200

    List of words

--- pendu.py
+++ pendu.py
@@ -1,3 +1,19 @@
+import random as rd
+
+words = "maman datascience competences voyage changement restaurant musee exposition"
+words_list = words.split()
+random_number = rd.randint(0, len(words_list) - 1)
+secret_word = words_list[random_number]
+
+game = {
+    "secret_word": secret_word,
+    "guess_word": "_ " * len(secret_word),
+    "nb_life": 10
+}
+print(secret_word)
+print(f"Lenght of the word : {len(game['secret_word'])}")
+print(f"{game['guess_word']} - Life : {game['nb_life']}")
+
 
 print("Bienvenu dans le jeu du pendu")
 print("pour arrêter le jeu : soit vous gagnez soit vous écrivez 'stop'")
