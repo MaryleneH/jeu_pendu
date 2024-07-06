63a60058b3fd85a1c4a4afa6e6f35d2fd669511d
MaryleneH <marylene.henry@gmail.com>
2024-07-06 12:36:31 +0200

    [INIT] start

---
+++ pendu.py
@@ -0,0 +1,8 @@
+
+print("Bienvenu dans le jeu du pendu")
+print("pour arrêter le jeu : soit vous gagnez soit vous écrivez 'stop'")
+
+while True:
+    mot = input(" ? > ")
+    if mot == "stop":
+        break
